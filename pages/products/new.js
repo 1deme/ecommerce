0f4d315d4e3c5af4e3e4dc453c0dcbc98e6482b4
demetre <demetrelabadze1@gmai.com>
2023-07-21 20:29:00 +0400

    visit add products page

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,16 +1,24 @@
 import Layout from "@/components/Layout";
 import axios from "axios";
 import { useState } from "react";
+import { useRouter } from "next/router";
 
 export default function NewProduct(){
     const [title,setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [price, setPrice] = useState('');
-
+    const [goToProducts, setgoToProducts] = useState(false);
+    const router = useRouter();
     async function createProduct(ev){
         ev.preventDefault();
         const data = {title, description, price};
-        await axios.post('/api/products', data)
+        await axios.post('/api/products', data);
+        setgoToProducts(true);
+
+    }
+
+    if(goToProducts){
+        router.push('/products');
     }
 
     return (
