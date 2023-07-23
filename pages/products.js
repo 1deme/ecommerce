d92383eee527b6de2516de6a14d27f7f65436f71
demetre <demetrelabadze1@gmai.com>
2023-07-24 01:55:48 +0400

    designe update

--- pages/products.js
+++ pages/products.js
@@ -12,8 +12,8 @@ export default function Products(){
     }, [])
     return (
         <Layout>
-            <Link className="bg-blue-900 text-white rounded-md py-1 px-2" 
-            href={'/products/new'}>add new product</Link>
+            <Link className="btn-primary" 
+            href={'/products/new'}>Add new product</Link>
             <table className="basic mt-2">
                 <thead>
                     <tr>
@@ -26,13 +26,13 @@ export default function Products(){
                         <tr key={product._id}>
                             <td>{product.title}</td>
                             <td>
-                                <Link href = {'/products/edit/' + product._id}>
+                                <Link className="btn-default" href = {'/products/edit/' + product._id}>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                                     </svg>
                                     Edit
                                 </Link>
-                                <Link href = {'/products/delete/'+product._id}>
+                                <Link className="btn-red" href = {'/products/delete/'+product._id}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 9.75L14.25 12m0 0l2.25 2.25M14.25 12l2.25-2.25M14.25 12L12 14.25m-2.58 4.92l-6.375-6.375a1.125 1.125 0 010-1.59L9.42 4.83c.211-.211.498-.33.796-.33H19.5a2.25 2.25 0 012.25 2.25v10.5a2.25 2.25 0 01-2.25 2.25h-9.284c-.298 0-.585-.119-.796-.33z" />
                                 </svg>
